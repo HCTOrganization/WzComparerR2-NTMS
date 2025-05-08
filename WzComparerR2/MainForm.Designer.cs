3419398d86c1547af1ad5f7a8f3772c62a481ce5
seotbeo <yoonv72@gmail.com>
2025-05-08 23:08:58 +0900

    StringLinker: Support updating via Language.wz

--- WzComparerR2/MainForm.Designer.cs
+++ WzComparerR2/MainForm.Designer.cs
@@ -252,9 +252,11 @@ namespace WzComparerR2
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.tsmi1Sort = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripSeparator();
             this.tsmi1Export = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmi1DumpAsXml = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmi1UpdateStringLinker = new System.Windows.Forms.ToolStripMenuItem();
             this.nodeConnector1 = new DevComponents.AdvTree.NodeConnector();
             this.elementStyle1 = new DevComponents.DotNetBar.ElementStyle();
             this.listViewExString = new DevComponents.DotNetBar.Controls.ListViewEx();
@@ -3035,6 +3037,11 @@ namespace WzComparerR2
             this.toolStripMenuItem4.Name = "toolStripMenuItem4";
             this.toolStripMenuItem4.Size = new System.Drawing.Size(151, 6);
             // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(151, 6);
+            // 
             // tsmi1Export
             // 
             this.tsmi1Export.Name = "tsmi1Export";
@@ -3053,6 +3060,12 @@ namespace WzComparerR2
             // 
             this.toolStripMenuItem5.Name = "toolStripMenuItem5";
             this.toolStripMenuItem5.Size = new System.Drawing.Size(151, 6);
+            // tsmi1UpdateStringLinker
+            // 
+            this.tsmi1UpdateStringLinker.Name = "tsmi1UpdateStringLinker";
+            this.tsmi1UpdateStringLinker.Size = new System.Drawing.Size(154, 22);
+            this.tsmi1UpdateStringLinker.Text = "更新StringLinker";
+            this.tsmi1UpdateStringLinker.Click += new System.EventHandler(this.tsmi1UpdateStringLinker_Click); 
             // 
             // elementStyle1
             // 
@@ -3584,6 +3597,7 @@ namespace WzComparerR2
         private DevComponents.DotNetBar.ComboBoxItem cmbItemAniNames;
         private DevComponents.DotNetBar.ButtonItem buttonItemUpdate;
         private System.Windows.Forms.ToolStripMenuItem tsmi1DumpAsXml;
+        private System.Windows.Forms.ToolStripMenuItem tsmi1UpdateStringLinker;
         private DevComponents.Editors.ComboItem comboItem18;
         private DevComponents.DotNetBar.Controls.CheckBoxX chkResolvePngLink;
         private DevComponents.DotNetBar.ComboBoxItem cmbItemSkins;
