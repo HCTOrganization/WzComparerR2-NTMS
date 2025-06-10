3323e7d4ca691a322ca75936c613a11cb83fec52
seotbeo <yoonv72@gmail.com>
2025-06-10 20:33:05 +0900

    Comparer: Fix set item rendering during comparison

--- WzComparerR2/Comparer/EasyComparer.cs
+++ WzComparerR2/Comparer/EasyComparer.cs
@@ -990,6 +990,7 @@ namespace WzComparerR2.Comparer
                 itemRenderNewOld[i].ShowSoldPrice = this.ShowPrice;
                 itemRenderNewOld[i].ShowCashPurchasePrice = this.ShowPrice;
                 itemRenderNewOld[i].ShowLinkedTamingMob = this.ShowLinkedTamingMob;
+                itemRenderNewOld[i].CompareMode = true;
             }
 
             foreach (var itemID in OutputItemTooltipIDs)
@@ -1171,6 +1172,7 @@ namespace WzComparerR2.Comparer
                 gearRenderNewOld[i].ShowSoldPrice = this.ShowPrice;
                 gearRenderNewOld[i].ShowCashPurchasePrice = this.ShowPrice;
                 gearRenderNewOld[i].ShowCombatPower = true;
+                gearRenderNewOld[i].CompareMode = true;
             }
 
             foreach (var gearID in OutputGearTooltipIDs)
@@ -1438,6 +1440,7 @@ namespace WzComparerR2.Comparer
                 gearRenderNewOld[i].ShowObjectID = this.ShowObjectID;
                 gearRenderNewOld[i].ShowSoldPrice = this.ShowPrice;
                 gearRenderNewOld[i].ShowCashPurchasePrice = this.ShowPrice;
+                gearRenderNewOld[i].CompareMode = true;
             }
 
             foreach (var gearID in OutputGearTooltipIDs)
