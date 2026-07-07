c7dc53b3a5274bfef929a50a14695c5cd65900c7
Calyx Hikari <29157608+HikariCalyx@users.noreply.github.com>
2026-07-07 22:43:44 +0800

    patcher: add keep old wz feature

--- WzComparerR2.CLI/CliPatcher.cs
+++ WzComparerR2.CLI/CliPatcher.cs
@@ -101,14 +101,15 @@ namespace WzComparerR2.CLI
             }
         }
 
-        public void ApplyPatch(string patchFile, string gameDirectory, bool immediatePatch, bool verbose)
+        public void ApplyPatch(string patchFile, string gameDirectory, bool immediatePatch, bool verbose, bool keepOldWz)
         {
             var session = new PatcherSession()
             {
                 PatchFile = patchFile,
                 MSFolder = gameDirectory,
                 PrePatch = immediatePatch,
-                DeadPatch = immediatePatch,
+                DeadPatch = immediatePatch && !keepOldWz,
+                KeepOldWz = keepOldWz,
             };
             session.LoggingFileName = Path.Combine(session.MSFolder, $"wcpatcher_{DateTime.Now:yyyyMMdd_HHmmssfff}.log");
             session.PatchExecTask = Task.Run(() => this.ExecutePatchAsync(session, session.CancellationToken, verbose));
@@ -288,6 +289,7 @@ namespace WzComparerR2.CLI
                 AppendStateText("應用更新\r\n");
                 var sw = Stopwatch.StartNew();
                 patcher.Patch(session.MSFolder, cancellationToken);
+                this.RestoreMissingDataFilesFromBackup(session, patcher.PatchParts, AppendStateText);
                 sw.Stop();
                 AppendStateText("結束\r\n");
                 session.State = PatcherTaskState.Complete;
@@ -331,6 +333,8 @@ namespace WzComparerR2.CLI
             public string CompareFolder;
             public bool PrePatch;
             public bool DeadPatch;
+            public bool KeepOldWz;
+            public bool KeepOldWzPrepared;
 
             public Task PatchExecTask;
             public string LoggingFileName;
@@ -453,6 +457,11 @@ namespace WzComparerR2.CLI
                     }
                     break;
                 case PatchingState.ApplyFile:
+                    if (session.KeepOldWz && !session.KeepOldWzPrepared)
+                    {
+                        this.PrepareKeepOldWz(session, logFunc);
+                        session.KeepOldWzPrepared = true;
+                    }
                     logFunc($"檔案應用: {e.Part.FileName}\r\n");
                     break;
                 case PatchingState.FileSkipped:
@@ -480,6 +489,142 @@ namespace WzComparerR2.CLI
             else
             {
                 return $"{size:N0} 位元組 ({targetbytes:0.##} {sizes[order]})";
+            }
+        }
+
+        private void PrepareKeepOldWz(PatcherSession session, Action<string> logFunc)
+        {
+            if (!session.KeepOldWz)
+            {
+                return;
+            }
+
+            string dataDir = Path.Combine(session.MSFolder, "Data");
+            string backupDir = Path.Combine(session.MSFolder, "DataBk");
+
+            if (!Directory.Exists(dataDir))
+            {
+                logFunc("KeepOldWz: Data 資料夾不存在，跳過。\r\n");
+                return;
+            }
+
+            if (Directory.Exists(backupDir))
+            {
+                logFunc("KeepOldWz: 既存的 DataBk 資料夾正在刪除中...\r\n");
+                RemoveReadOnlyAttributesRecursively(backupDir);
+                Directory.Delete(backupDir, true);
+            }
+
+            logFunc("KeepOldWz: Data 資料夾正在重新命名為 DataBk...\r\n");
+            Directory.Move(dataDir, backupDir);
+            logFunc("KeepOldWz: 完了\r\n");
+        }
+
+        private void RestoreMissingDataFilesFromBackup(PatcherSession session, IEnumerable<PatchPartContext> patchParts, Action<string> logFunc)
+        {
+            if (!session.KeepOldWz)
+            {
+                return;
+            }
+
+            string dataDir = Path.Combine(session.MSFolder, "Data");
+            string backupDir = Path.Combine(session.MSFolder, "DataBk");
+            if (!Directory.Exists(backupDir))
+            {
+                logFunc("KeepOldWz: DataBk 資料夾不存在，跳過補齊缺失的檔案。\r\n");
+                return;
+            }
+
+            Directory.CreateDirectory(dataDir);
+            var deletedFiles = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            var deletedDirs = new List<string>();
+            foreach (var part in patchParts.Where(p => p.Type == 2))
+            {
+                var fileName = (part.FileName ?? string.Empty).Replace('/', '\\');
+                if (!fileName.StartsWith("Data\\", StringComparison.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
+
+                var relativePath = fileName.Substring("Data\\".Length).TrimEnd('\\');
+                if (string.IsNullOrEmpty(relativePath))
+                {
+                    continue;
+                }
+
+                if (fileName.EndsWith("\\", StringComparison.Ordinal))
+                {
+                    deletedDirs.Add(relativePath);
+                }
+                else
+                {
+                    deletedFiles.Add(relativePath);
+                }
+            }
+
+            int copiedCount = 0;
+            foreach (string backupFile in Directory.EnumerateFiles(backupDir, "*", SearchOption.AllDirectories))
+            {
+                string relativePath = backupFile.Substring(backupDir.Length)
+                    .TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar)
+                    .Replace('/', '\\');
+                if (deletedFiles.Contains(relativePath))
+                {
+                    continue;
+                }
+                if (deletedDirs.Any(dir => relativePath.Equals(dir, StringComparison.OrdinalIgnoreCase)
+                    || relativePath.StartsWith(dir + "\\", StringComparison.OrdinalIgnoreCase)))
+                {
+                    continue;
+                }
+
+                string targetFile = Path.Combine(dataDir, relativePath);
+                if (File.Exists(targetFile))
+                {
+                    continue;
+                }
+
+                string targetDir = Path.GetDirectoryName(targetFile);
+                if (!string.IsNullOrEmpty(targetDir))
+                {
+                    Directory.CreateDirectory(targetDir);
+                }
+                File.Copy(backupFile, targetFile, false);
+                copiedCount++;
+            }
+
+            logFunc($"KeepOldWz: DataBk 資料夾中已複製 {copiedCount} 個缺失的檔案。\r\n");
+        }
+
+        private static void RemoveReadOnlyAttributesRecursively(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                return;
+            }
+
+            foreach (string filePath in Directory.EnumerateFiles(directoryPath, "*", SearchOption.AllDirectories))
+            {
+                var attr = File.GetAttributes(filePath);
+                if ((attr & FileAttributes.ReadOnly) != 0)
+                {
+                    File.SetAttributes(filePath, attr & ~FileAttributes.ReadOnly);
+                }
+            }
+
+            foreach (string dirPath in Directory.EnumerateDirectories(directoryPath, "*", SearchOption.AllDirectories))
+            {
+                var attr = File.GetAttributes(dirPath);
+                if ((attr & FileAttributes.ReadOnly) != 0)
+                {
+                    File.SetAttributes(dirPath, attr & ~FileAttributes.ReadOnly);
+                }
+            }
+
+            var rootAttr = File.GetAttributes(directoryPath);
+            if ((rootAttr & FileAttributes.ReadOnly) != 0)
+            {
+                File.SetAttributes(directoryPath, rootAttr & ~FileAttributes.ReadOnly);
             }
         }
 
--- WzComparerR2/FrmPatcher.Designer.cs
+++ WzComparerR2/FrmPatcher.Designer.cs
@@ -63,7 +63,7 @@ namespace WzComparerR2
             this.chkOutputRemovedImg = new DevComponents.DotNetBar.Controls.CheckBoxX();
             this.chkOutputAddedImg = new DevComponents.DotNetBar.Controls.CheckBoxX();
             this.cmbComparePng = new DevComponents.DotNetBar.Controls.ComboBoxEx();
-            this.chkOutputPng = new DevComponents.DotNetBar.Controls.CheckBoxX();
+            this.chkKeepOldWz = new DevComponents.DotNetBar.Controls.CheckBoxX();
             this.chkCompare = new DevComponents.DotNetBar.Controls.CheckBoxX();
             this.panelEx2 = new DevComponents.DotNetBar.PanelEx();
             this.superTabControl1 = new DevComponents.DotNetBar.SuperTabControl();
@@ -363,8 +363,8 @@ namespace WzComparerR2
             //this.expandablePanel2.Controls.Add(this.chkOutputRemovedImg);
             //this.expandablePanel2.Controls.Add(this.chkOutputAddedImg);
             //this.expandablePanel2.Controls.Add(this.cmbComparePng);
-            //this.expandablePanel2.Controls.Add(this.chkOutputPng);
             //this.expandablePanel2.Controls.Add(this.chkCompare);
+            this.expandablePanel2.Controls.Add(this.chkKeepOldWz);
             this.expandablePanel2.Controls.Add(this.panelEx2);
             this.expandablePanel2.Controls.Add(this.buttonXPatch);
             this.expandablePanel2.Controls.Add(this.chkDeadPatch);
@@ -501,29 +501,26 @@ namespace WzComparerR2
             this.superTooltip1.SetSuperTooltip(this.cmbComparePng, superTooltipInfo9);
             this.cmbComparePng.TabIndex = 12;
             // 
-            // chkOutputPng
+            // chkKeepOldWz
             // 
-            this.chkOutputPng.AutoSize = true;
-            this.chkOutputPng.BackColor = System.Drawing.Color.Transparent;
+            this.chkKeepOldWz.AutoSize = true;
+            this.chkKeepOldWz.BackColor = System.Drawing.Color.Transparent;
             // 
             // 
             // 
-            this.chkOutputPng.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.chkOutputPng.Checked = true;
-            this.chkOutputPng.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkOutputPng.CheckValue = "Y";
-            this.chkOutputPng.Location = new System.Drawing.Point(208, 111);
-            this.chkOutputPng.Name = "chkOutputPng";
-            this.chkOutputPng.Size = new System.Drawing.Size(85, 16);
-            this.chkOutputPng.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
-            superTooltipInfo10.BodyText = "PNGファイルと音声ファイルを表示します。";
+            this.chkKeepOldWz.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.chkKeepOldWz.Location = new System.Drawing.Point(200, 87);
+            this.chkKeepOldWz.Name = "chkKeepOldWz";
+            this.chkKeepOldWz.Size = new System.Drawing.Size(85, 16);
+            this.chkKeepOldWz.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            superTooltipInfo10.BodyText = "這是「WZ比較」功能專用的特別補丁應用模式。\r\n使用此模式時，舊的WZ文件不會被新的WZ文件覆蓋，而是會移動到遊戲目錄內的DataBk目錄中。\r\n\r\n啟用此模式後，「即時更新」將無法使用。";
             superTooltipInfo10.Color = DevComponents.DotNetBar.eTooltipColor.System;
-            superTooltipInfo10.CustomSize = new System.Drawing.Size(180, 80);
+            superTooltipInfo10.CustomSize = new System.Drawing.Size(250, 180);
             superTooltipInfo10.FooterVisible = false;
-            superTooltipInfo10.HeaderText = "PNGと音声";
-            this.superTooltip1.SetSuperTooltip(this.chkOutputPng, superTooltipInfo10);
-            this.chkOutputPng.TabIndex = 11;
-            this.chkOutputPng.Text = "PNGと音声";
+            superTooltipInfo10.HeaderText = "保留舊版WZ檔";
+            this.superTooltip1.SetSuperTooltip(this.chkKeepOldWz, superTooltipInfo10);
+            this.chkKeepOldWz.TabIndex = 11;
+            this.chkKeepOldWz.Text = "保留舊版WZ檔";
             // 
             // chkCompare
             // 
@@ -1027,7 +1024,7 @@ namespace WzComparerR2
         private DevComponents.DotNetBar.ButtonX buttonXCheck;
         private DevComponents.AdvTree.ColumnHeader columnHeader6;
         private DevComponents.DotNetBar.Controls.ComboBoxEx cmbComparePng;
-        private DevComponents.DotNetBar.Controls.CheckBoxX chkOutputPng;
+        private DevComponents.DotNetBar.Controls.CheckBoxX chkKeepOldWz;
         private DevComponents.DotNetBar.SuperTooltip superTooltip1;
         private DevComponents.DotNetBar.ExpandablePanel expandablePanel3;
         private DevComponents.DotNetBar.LabelX labelX3;
